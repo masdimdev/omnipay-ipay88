ed7524b35cd63d5624695015de2cbd9fe423fa81
Dimas <me@masdim.dev>
2025-01-18 00:41:29 +0700

    fix: fix complete purchase

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -2,41 +2,25 @@
 
 namespace Omnipay\Ipay88\Message;
 
-use Exception;
-
 class CompletePurchaseRequest extends AbstractRequest
 {
-    /**
-     * @throws Exception
-     */
     public function getData()
     {
-        $data = $this->httpRequest->request->all();
-
-        $status = (string)($data['Status'] ?? '');
-        if ($status !== '1') {
-            throw new Exception($data['ErrDesc'] ?? 'An error occurred.');
-        }
-
-        $signature = $data['Signature'] ?? '';
-        $generatedSignature = $this->generateSignature([
-            $this->getMerchantKey(),
-            $data['MerchantCode'] ?? '',
-            $data['RefNo'] ?? '',
-            $data['Amount'] ?? '',
-            $data['Currency'] ?? '',
-            $data['Status'] ?? '',
-        ]);
-
-        if ($signature !== $generatedSignature) {
-            throw new Exception('Invalid signature.');
-        }
-
-        return $data;
+        return $this->httpRequest->request->all();
     }
 
     public function sendData($data): CompletePurchaseResponse
     {
-        return $this->response = new CompletePurchaseResponse($this, $data);
+        $generatedSignature = $this->generateSignature([
+            $this->getMerchantKey(),
+            $data['MerchantCode'] ?? '',
+            $data['PaymentId'] ?? '',
+            $data['RefNo'] ?? '',
+            preg_replace('/[.,]/', '', (string)($data['Amount'] ?? '')),
+            $data['Currency'] ?? '',
+            $data['Status'] ?? '',
+        ]);
+
+        return $this->response = new CompletePurchaseResponse($this, $data, $generatedSignature);
     }
 }
--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -3,12 +3,27 @@
 namespace Omnipay\Ipay88\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RequestInterface;
 
 class CompletePurchaseResponse extends AbstractResponse
 {
+    protected string $generatedSignature;
+
+    public function __construct(RequestInterface $request, $data, $generatedSignature)
+    {
+        parent::__construct($request, $data);
+
+        $this->generatedSignature = $generatedSignature;
+    }
+
     public function isSuccessful(): bool
     {
         return (string)($this->data['Status'] ?? '') === '1';
+    }
+
+    public function isSignatureValid(): bool
+    {
+        return (string)($this->data['Signature'] ?? '') === $this->generatedSignature;
     }
 
     public function getTransactionId()
